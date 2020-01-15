ed2531ec6560727253499d03a18e6a018b83fe38
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2020-01-15 10:50:13 +0530

    Node.JS-Day-1

--- Node.JS/Node/2.blocking-IO.js
+++ Node.JS/Node/2.blocking-IO.js
@@ -1,8 +1,13 @@
 // neeed a module to read file from disk . we use require keyword to inport node package
 var fs=require('fs'); // file systems
 
+function test(){
+    console.log('Hello...');
+    
+}
+
 function io(){
-    var vegMenu=fs.readFile('./veg-menu.txt','utf8',(error,myMemu)=>{
+    var vegMenu=fs.readFileSync('./veg-menu.txt','utf8',(error,myMemu)=>{
         if(error){
             console.log('problem loading file');
             return;
@@ -12,7 +17,7 @@ function io(){
     });
 }
 io();
-
+setTimeout(test,3000);
 console.log('--- do something else');
 
 
