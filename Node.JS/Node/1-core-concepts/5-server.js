dad667cf1b5c342a66ff92b94cab96c856113378
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2020-01-20 15:55:16 +0530

    Node-Final-session

--- Node.JS/Node/1-core-concepts/5-server.js
+++ Node.JS/Node/1-core-concepts/5-server.js
@@ -2,11 +2,11 @@
 
 // To use the HTTP server and client one must require('http').
 
-const myProtocal=require('http');
+const http=require('http');
 const fs=require('fs');
 
 
-const server=myProtocal.createServer(); // webserver => event-emiter
+const server=http.createServer(); // webserver => event-emiter
 
 // every server must run on a port number.
 
@@ -38,6 +38,8 @@ server.on('request',(request,response)=>{
     response.end();
 }) */
 
+
+/*
 // making response type as 'pdf'
 server.on('request',(req,res)=>{
     res.writeHead(200,{'Content-Type':'application/pdf'});
@@ -64,16 +66,127 @@ server.on('request',(req,res)=>{
 // Nodemon instalation procedure
 
 // Visit https://nodemon.io/
-// open ythe console 2ith admin privileges and provide following command 
+// open the console with admin privileges and provide following command 
 // npm install -g nodemon
 // after instalation is completed, then run node server file with nodemon command
 // ex: nodemon 5-server.js
 
 //----------------------------------------------------------------------------------
   
+*/
+/*
+server.on('request',(req,res)=>{
+  
+    console.log('HTTP Request type: '+req.method);
+    console.log('HTTP Request URl: '+req.url);
+
+    let httpMethod=req.method;
+    let httpUrl=req.url;
+
+    console.log('Hello '+req.name);
+    
+    /*
+    if(httpMethod == 'GET' && httpUrl=='/'){
+        console.log('You have sent GET request for /'); 
+      //  console.log('hello '+req.query.name);
+        
+    }
+
+    if(httpMethod == 'POST' && httpUrl=='/'){
+        console.log('You have sent POST request for /'); 
+    }
+
+    if(httpMethod == 'PUT' && httpUrl=='/'){
+        console.log('You have sent PUT request for /'); 
+    }
+
+    if(httpMethod == 'DELETE' && httpUrl=='/'){
+        console.log('You have sent DELETE request for /'); 
+    }
+    
+   
 
 
+    res.end();
+    
+});
+
+ */
 
 
+ /*
+Video gets loaded into a buffer meory (Ram Memory)
+   30 MB -> 100 X 30 MB -> ? 
 
+
+ */
+
+ /*
+ // Stream a video file with out chunks
+server.on('request',(req,res)=>{
+    res.writeHead(200,{'Content-Type':'video/mp4'});
+    fs.readFile('./20191004_195205.mp4',(err,data)=>{
+        if(err){
+            console.log('error occured '+err);
+            return;
+        }
+
+        res.write(data);
+        console.log(data);
+        
+    })
+
+});
+
+*/
+
+/*
+server.on('request',(req,res)=>{
+
+    res.writeHead(200,{'Content-Type':'video/mp4'});
+    let readStream=fs.createReadStream('./20191004_195205.mp4');// EventEmitter
+
+    readStream.on('data',(chunk)=>{
+        res.write(chunk);
+        console.log(chunk);
+        
+    });
+
+    readStream.on('end',()=>{
+        res.end();
+
+    })
+
+
+})
+
+*/
+
+server.on('request',(request,response)=>{
+
+    const requestUrl=request.url;
+    console.log(request.url);
+    
+    if(requestUrl=='/employees'){
+        response.write('Emloyees request is processing')
+        response.end();
+        return;
+    }
+
+    if(requestUrl=='/products'){
+        response.write('Product request is processing')
+        response.end();
+        return;
+    }
+
+    if(requestUrl=='/todos'){
+        response.write('Todos request is processing')
+        response.end();
+        return;
+    }
+
+    response.write('General Reuqets is processing');
+    response.end();
+
+})
 
