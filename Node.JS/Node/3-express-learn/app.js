c5749b971b775d6a33321c750f7cec9faa7995ad
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2020-01-22 16:11:22 +0530

    Express-JS Middleware

--- Node.JS/Node/3-express-learn/app.js
+++ Node.JS/Node/3-express-learn/app.js
@@ -48,8 +48,9 @@ app.get('/view',(req,res)=>{
 
 let fs=require('fs');
 
+/*
 app.get('/',(req,res)=>{
-    res.writeHead(200,{'Comtent-Type':'text/html'});
+   // res.writeHead(200,{'Comtent-Type':'text/html'});
    /*  fs.readFile('./public/index.html',(err,data)=>{
         res.write(data);
         res.end();
@@ -62,19 +63,170 @@ app.get('/',(req,res)=>{
     });
     stream.on('end',()=>{
         res.end();
-    }) */
+    }) 
 
     // or
-    fs.createReadStream('./public/index.html').pipe(res);
+   // fs.createReadStream('./public/index.html').pipe(res);
+   res.sendFile(__dirname+'/public/index.html');
 })
 
 // for css
 app.get('/css/bootstrap.min.css',(req,res)=>{
-    fs.createReadStream('./public/css/bootstrap.min.css').pipe(res);
+    //fs.createReadStream('./public/css/bootstrap.min.css').pipe(res);
+    res.sendFile(__dirname+'/public/css/bootstrap.min.css');
 })
+*/
+
+
+/* app.use('/test',(req,res,next)=>{
+    console.log('step-1'); 
+    next();
+})
+
+app.use('/test',(req,res,next)=>{
+    console.log('step-2'); 
+    next();
+})
+
+app.use('/test',(req,res,next)=>{
+    console.log('step-3'); 
+    next();
+})
+
+app.use('/test',(req,res,next)=>{
+   res.send('Hello test is completed...');
+})
+ */
+
+ /* 
+app.use((req,res,next)=>{
+    console.log('Validation');
+    next();
+})
+
+app.use((req,res,next)=>{
+   console.log('Authentication');
+   next();
+})
+
+
+ app.use('/cts',(req,res,next)=>{
+     //res.send('Hello cts associate!');
+     // custom BU logic
+     console.log('cts request BU logic is going on..');
+     next();
+ })
+
+
+ app.use('/cts',(req,res,next)=>{
+    res.send('Hello cts associate!');
+   console.log('cts request proccessed.....');
+   
+})
+
+
+app.use("/saveProduct",(req,res,next)=>{
+    console.log('Data Parsing');
+    next();
+   //res.send('data parsing issue');
+})
+
+app.use("/saveProduct",(req,res)=>{
+    // logic to save product to DB
+   res.send('Product saved');
+   console.log('-- saving product to db is completed...');
+   
+})
+
+
+app.get('/products',(req,res,next)=>{
+    console.log('-- getting producs step 1'); 
+    next();
+})
+
+app.get('/products',(req,res,next)=>{
+    res.send(['Item-1','Item-2','Item-3','Item-4'])
+    console.log('-- loading products completed'); 
+})
+
+ */
+
+app.use('/sample',(req,res,next)=>{
+    res.send('Will execute for every reqiest');
+});
+
+
+
+app.get('/data',(req,res)=>{
+
+    res.send('Only with get request');
+});
+
+app.put('/data',(req,res)=>{
+
+    res.send('Only with PUT request');
+});
+
+app.delete('/data',(error,req,res)=>{
+    throw error;
+    res.send('Only with Dleete request');
+
+});
+
+app.post('/data',(req,res)=>{
+
+    res.send('Only with POST request');
+});
+
+
+/* app.get('/sample',(req,res)=>{
+
+    res.send('Only with get request');\
+}); */
+
+
+//app.use(express.static("c:/abc"));
+app.use(express.static(__dirname+'/public')) // loads only from public
+//app.use(express.static(__dirname+'/src')) // only from src
+//app.use(express.static(__dirname+'.')) // loads all folder data
+
+//let num=require('c:/abc/test');
+app.use('/todos/:name',(req,res,next)=>{
+    if(req.params.name.length<=3){
+        res.send('User name must have grater than 3 letters');
+        return;
+    }
+    next();
+})
+
+app.use('/todos/:name',(req,res,next)=>{
+    res.send('Your name is validated.. '+req.params.name)
+})
+/* 
+app.get('/todos',(req,res)=>{
+    res.send('Uuser namme must be provied...');
+})
+app.get('/todos/:userName',(req,res)=>{
+    let user=req.params.userName;
+    if(user.length<=3){
+        console.log('Invalid user name');
+        res.send('Invalid user name')
+        return;
+    }
+    res.send('hello dear user '+user)
+    console.log('Usrr name is '+user);
+}) */
+
+
+
+
+
+
 
 app.listen(3000,()=>{
     console.log('aserver running on http://localhost:3000');
 })
+
+
 
 
