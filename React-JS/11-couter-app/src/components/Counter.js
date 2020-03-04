e8854fdb80709d277399a377c3ee0fcdf14a711a
praveen.somireddy@gmail.com <praveen.somireddy@gmail.com>
2020-03-04 15:50:36 +0530

    bug fix

--- React-JS/11-couter-app/src/components/Counter.js
+++ React-JS/11-couter-app/src/components/Counter.js
@@ -1,43 +1,53 @@
 import React, { Component } from 'react';
-
 class Counter extends Component {
-    state={
-        count:0,
-        isEdit:false
+    state = {
+        count: 0,
+        isEdit: false
     }
-    calculateTotalHits(){
-        let {count}=this.state;
-        this.setState({count:count+1},()=>{
-            let {onAction}=this.props;
-            onAction({value:this.props.lable}); // child invokes parents onAction's property value
+    calculateTotalHits() {
+        let { count } = this.state;
+        this.setState({ count: count + 1 }, () => {
+            let { onAction } = this.props;
+            onAction({ value: this.props.lable }); // child invokes parents onAction's property value
         })
     }
 
-    renderEdit(){
-        console.log('--- renderedit '+this.props.lable);
-        this.setState({isEdit:true})
+    renderEdit() {
+        console.log('--- renderedit ' + this.props.lable);
+        this.setState({ isEdit: true })
     }
 
-    deleteComponent(){
-        console.log('delete component '+this.props.lable);
-        
+    deleteComponent(e) {
+        let { removeAction } = this.props;
+        removeAction({ value: this.props.lable, count: this.state.count });
+        this.setState({ count: 0 })
+
+    }
+    changeLable(e) {
+        let key = e.which || e.keyCode;
+        if (key === 13) {
+            //console.log(e.target.value);
+            let { addComponent } = this.props;
+            this.setState({ isEdit: false })
+            addComponent({ oldLable: this.props.lable, newLable: e.target.value, count: this.state.count })
+        }
+
     }
     render() {
-        let {isEdit}=this.state;
+        let { isEdit } = this.state;
         return (
             <div className="mystyle">
-             {isEdit?<input type="text" size="3"/>:<span onDoubleClick={e=>this.renderEdit()}>{this.props.lable}</span>}   
-             
-               <button className="btn btn-primary" onClick={e=>this.calculateTotalHits()}>{this.state.count}</button>
-              <span style={{margin:'3px'}}>
-              <i className="fas fa-trash-alt" onClick={e=>this.deleteComponent()}/>	
-              </span>
+                {isEdit ? <input type="text" size="3" onKeyPress={e => this.changeLable(e)} /> : <span onDoubleClick={e => this.renderEdit()}>{this.props.lable}</span>}
+                <button className="btn btn-primary" onClick={e => this.calculateTotalHits()}>{this.state.count}</button>
+                <span style={{ margin: '3px',color:'red',fontSize:'18px' }} >
+                    <i className="fas fa-trash-alt" onClick={e => this.deleteComponent(e)} />
+                </span>
             </div>
         );
     }
 
-    componentWillUnmount(){
-        console.log('--- Component Removed'); 
+    componentWillUnmount() {
+        console.log('--- Component Removed');
     }
 
 }
--- React-JS/11-couter-app/src/components/CounterBox.js
+++ React-JS/11-couter-app/src/components/CounterBox.js
@@ -6,42 +6,84 @@ class CounterBox extends Component {
 
     state = {
         total: 0,
-        buttons:[10,20,-5,-20,100]
+        buttons: [10, 20, -5, -20, 100]
     }
 
     calculateTotal(e) {
         let { total } = this.state;
-        this.setState ({ total: total + e.value })
+        this.setState({ total: total + e.value })
     }
 
-    renderButtons(){
-        let {buttons}=this.state;
-        return buttons.map((btn,idx)=>{
-           return(
-            <div key={idx} className="alert alert-info myspace">
-            <Counter lable={btn} onAction={e =>this.calculateTotal(e)} />
-            </div>
-           ); 
+    removeComponent(e) {
+        let { total } = this.state;
+
+        console.log(e.count);
+        let { buttons } = this.state;
+        let newButtons = buttons.filter(button => button !== e.value);
+        console.log(buttons);
+        console.log(newButtons);
+        this.setState({ buttons: newButtons, total: total - (e.value * e.count) });
+
+    }
+
+    renderButtons() {
+        let { buttons } = this.state;
+        return buttons.map((btn, idx) => {
+            return (
+                <div key={idx} className="alert alert-info col-2 myspace">
+                    <Counter lable={btn} onAction={e => this.calculateTotal(e)}
+                        removeAction={e => this.removeComponent(e)}
+                        addComponent={e => this.editComponent(e)}
+                    />
+                </div>
+            );
         })
     }
 
-    addComponent(){
-        console.log('Value: '); 
+    editComponent(e) {
+
+        let { buttons, total } = this.state;
+        let oldLable = Number.parseInt(e.oldLable);
+        let newLable = Number.parseInt(e.newLable);
+
+        let status = buttons.includes(newLable);
+        buttons.map((button, idx) => (!status) ? (button === oldLable && !(isNaN(newLable))) ? buttons[idx] = newLable : buttons[idx] = oldLable : null);
+        if (!(isNaN(newLable))){
+            this.setState({ buttons: buttons, total: (total + (e.componentLable * e.count)) - e.oldLable * e.count })
+        }
+    }
+    addNewComponent(e) {
+        let key = e.which || e.keyCode;
+        if (key === 13) {
+            let { buttons } = this.state;
+            let newLable = Number.parseInt(e.target.value);
+            let status = buttons.includes(newLable);
+            if (!status && !(isNaN(newLable))) {
+                buttons.push(newLable);
+            }
+
+            this.setState({ buttons: buttons })
+        }
+
     }
 
     render() {
 
         return (
             <div className="card">
-                <div className="card-header"><h4>Counter-APP</h4></div>
-                <input className="col-2"   onBlur={e=>this.addComponent()}/>
-                <div className="card-body row"> 
-               
-                   {this.renderButtons()}
+                <div className="card-header text-center"><h4>Counter-APP</h4></div>
+                <div className="form-inline mt-3" style={{ alignSelf: 'center' }}>
+                    <input className="form-control mr-1" onKeyPress={e => this.addNewComponent(e)} placeholder="New Lable" />
+                    <button className="btn btn-danger" >Add</button>
+                </div>
+                <hr />
+                <div className="card-body row">
+
+                    {this.renderButtons()}
                 </div>
 
-                <div className="card-footer">
-                    <h4>Total Sum:  {this.state.total}</h4>
+                <div className="card-footer text-center">
+                    <h4>Total Sum: <span className="badge badge-dark">{this.state.total}</span> </h4>
                 </div>
 
             </div>
